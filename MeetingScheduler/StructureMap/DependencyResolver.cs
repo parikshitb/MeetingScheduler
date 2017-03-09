50351ddf7df5d2f45540b8631359da98c22f5c26
parikshit <parixit23@gmail.com>
2017-03-09 20:20:17 +0530

    initial commit project structure (half)

---
+++ MeetingScheduler/StructureMap/DependencyResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeetingScheduler.StructureMap
+{
+    public class DependencyResolver
+    {
+    }
+}
